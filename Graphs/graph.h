020eb37b45530e3461e25e91bf176950006d5c20
Asad <asad.waheed.23@gmail.com>
2016-02-04 15:57:19 -0600

    Header file

---
+++ Graphs/graph.h
@@ -0,0 +1,15 @@
+// All prototypes we need to make a graph 
+// can be found below
+// gin n juice
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+// data
+#define MAX 26
+//functions
+int dirGraph();
+int undirGraph();
+int readGraph( int adjacency_matrix[MAX][MAX], int count);
+
+#endif
